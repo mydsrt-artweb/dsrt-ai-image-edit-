2a6c4304df497275c1c4a97a3f9f846f56d0323c
mydsrt-artweb <my.project944@gmail.com>
2025-09-16 21:00:47 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,14 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  experimental: {
-    appDir: true
-  }
-}
-module.exports = nextConfig
+-/** @type {import('next').NextConfig} */
+-const nextConfig = {
+-  reactStrictMode: true,
+-  experimental: {
+-    appDir: true
+-  }
+-}
+-module.exports = nextConfig
++/** @type {import('next').NextConfig} */
++const nextConfig = {
++  reactStrictMode: true,
++}
++
++module.exports = nextConfig
