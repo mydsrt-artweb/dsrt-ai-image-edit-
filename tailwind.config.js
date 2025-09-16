b0cf76e7ab9d01481e0549330ed433a88d3e2fc9
mydsrt-artweb <my.project944@gmail.com>
2025-09-16 21:48:22 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,5 @@
 module.exports = {
-  content: [
-    "./app/**/*.{js,jsx,ts,tsx}",
-    "./components/**/*.{js,jsx,ts,tsx}",
-    "./pages/**/*.{js,jsx,ts,tsx}"
-  ],
-  theme: {
-    extend: {}
-  },
+  content: ["./app/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  theme: { extend: {} },
   plugins: []
 }
