43e50a99887f615ef8b3150b5c35d91a3ae4f568
mydsrt-artweb <my.project944@gmail.com>
2025-09-16 21:20:32 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,14 +1,14 @@
--/** @type {import('next').NextConfig} */
--const nextConfig = {
--  reactStrictMode: true,
--  experimental: {
--    appDir: true
--  }
--}
--module.exports = nextConfig
-+/** @type {import('next').NextConfig} */
-+const nextConfig = {
-+  reactStrictMode: true,
-+}
-+
-+module.exports = nextConfig
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**", // boleh ambil image dari mana saja
+      },
+    ],
+  },
+}
+
+export default nextConfig
